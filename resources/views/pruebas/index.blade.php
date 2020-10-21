f96fb3900d503c46f53f66ba8679f8118282547e
marvinero <marvinero@gmail.com>
2020-10-21 16:27:29 -0400

    integracion bootstrap

--- resources/views/pruebas/index.blade.php
+++ resources/views/pruebas/index.blade.php
@@ -1,13 +1,54 @@
-<div class="container">
-    <div class="row">
-      <div class="col-sm">
-        One of three columns
-      </div>
-      <div class="col-sm">
-        One of three columns
-      </div>
-      <div class="col-sm">
-        One of three columns
-      </div>
-    </div>
+<head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+
+<body>
+<header>
+  <div class="card">
+    <h5 class="card-header">Formulario</h5>    
+    
   </div>
+  
+</header>  
+<br>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">  
+                <div class="card-body">
+                  <form>                    
+                    <div class="form-group">
+                      <label for="">Nombres</label>
+                      <input type="text" class="form-control" id="" placeholder="Ingrese su nombre(s)">
+                    </div>
+                    <div class="form-group">
+                      <label for="">Apellidos</label>
+                      <input type="text" class="form-control" id="" placeholder="Ingrese su apellido(s)">
+                    </div> 
+                    <div class="form-group">
+                      <label for="">C.I.</label>
+                      <input type="number" class="form-control" id="" placeholder="Ingrese su CI">
+                    </div>  
+                    <div class="form-group">
+                      <label for="">Telefono</label>
+                      <input type="number" class="form-control" id="" placeholder="Ingrese su telefono">
+                    </div>  
+                    <div class="form-row">
+                      <div class="form-group col-md-6">
+                        <label for="">Email</label>
+                        <input type="email" class="form-control" id="" placeholder="Ingrese su email">
+                      </div>                      
+                    </div>           
+                </div> 
+                    <div class="btn-group" role="group" aria-label="Basic example">
+                      <button type="button" class="btn btn-outline-primary">Aceptar</button>                      
+                      <button type="button" class="btn btn-outline-danger">Cancelar</button>
+                    </div>
+                  </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
